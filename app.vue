5f548a060a76043be43a3baabce4a97b7810173f
Austin73 <maneeshpal85@gmail.com>
2024-08-19 16:02:44 +0530

    init: repo

---
+++ app.vue
@@ -0,0 +1,12 @@
+
+import { NuxtPage } from '#build/components';
+
+import { NuxtLayout } from '#build/components';
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  </div>
+</template>
